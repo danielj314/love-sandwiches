4d20a15a35dbb7cb046e8d64fc23a8c1651c3806
danielj314 <danieljones314@gmail.com>
2022-08-30 00:07:59 +0000

    Created get_stock_values function to print recommendation on stock numbers for next event in terminal.

--- run.py
+++ run.py
@@ -118,6 +118,23 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
+
+    headings = SHEET.worksheet('stock').row_values(1)
+
+    print("Make the following numbers of sandwiches for next market:\n")
+
+    new_data = {}
+    for heading, stock_num in zip(headings, data):
+        new_data[heading] = stock_num
+    return new_data
+    
+    # return {heading: data for heading, data in zip(headings, data)}
+
+
 def main():
     """
     Run all program functions
@@ -130,7 +147,9 @@ def main():
     sales_columns = get_last_5_entires_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
-    
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
+
 
 print("Welcome to love sandwiches data automation!")
 main()
